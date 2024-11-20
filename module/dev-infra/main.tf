c97618f034b30917b4ef8fa88645357cfc976e5c
Prateekkumawat <prateekkumawat20@gmail.com>
2024-11-20 18:15:38 +0530

    update dev code

--- module/dev-infra/main.tf
+++ module/dev-infra/main.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "dev-vpc" {
   cidr_block = var.aws_dev_vpc_cidr
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = var.aws_dev_vpc_subnet_cidr
+  availability_zone = "ap-south-1"
+  vpc_id = aws_vpc.dev-vpc.id
+}
