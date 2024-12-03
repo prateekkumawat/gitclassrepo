6dcfda773af1e092a3f45650ef9a173dacd350bc
Prateekkumawat <prateek.kumawat@mynxsoftwares.com>
2024-12-03 18:21:38 +0530

    typo error

--- src/index.php
+++ src/index.php
@@ -6,6 +6,20 @@
 <h1>add new line code </h1>
 <?php
 echo "Hello World!";
+
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
 
 </body>
